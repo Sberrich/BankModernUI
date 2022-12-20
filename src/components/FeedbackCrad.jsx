ea13523d80d00a29357316402a8a311dbe68449b
Berrichi Samir <49416974+Sberrich@users.noreply.github.com>
2022-12-20 19:28:08 +0100

    bankmodernui

---
+++ src/components/FeedbackCrad.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FeedbackCrad = () => {
+  return (
+    <div>FeedbackCrad</div>
+  )
+}
+
+export default FeedbackCrad
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import {useState} from 'react'
+import {close, logo, menu} from '../assets'
+import { navLinks } from '../constants/constants'
+
+
+const Navbar = () => {
+  return (
+    <div className='w-full flex py-6 justify-between items-center navbar'>
+      <img/>
+    </div>
+  )
+}
+
+export default Navbar
