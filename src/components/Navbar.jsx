e59a91ed0f574822c98efc9d166bec046b42f273
Berrichi Samir <49416974+Sberrich@users.noreply.github.com>
2022-12-20 20:21:40 +0100

    Navbar donne

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,55 @@
-import {useState} from 'react'
-import {close, logo, menu} from '../assets'
-import { navLinks } from '../constants/constants'
-
+import { useState } from "react";
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants/constants";
 
 const Navbar = () => {
-  return (
-    <div className='w-full flex py-6 justify-between items-center navbar'>
-      <img/>
-    </div>
-  )
-}
+  const [toggle, setToggle] = useState(false);
 
-export default Navbar
+  return (
+    <div className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            } text-white`}
+            key={nav.id}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-4 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex flex-col  justify-end items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                className={`font-poppins font-normal cursor-pointer text-[16px] ${
+                  index === navLinks.length - 1 ? "mb-0" : "mb-4"
+                } text-white`}
+                key={nav.id}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
