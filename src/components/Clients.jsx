501782bf5eafaa808dc288dcdfd7d5fcb2d6abb2
Berrichi Samir <49416974+Sberrich@users.noreply.github.com>
2022-12-23 17:40:48 +0100

    clients in progress

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import {clients}  from "../constants/constants"
+import styles from "../style"
 
-const Clients = () => {
-  return (
-    <div>Clients</div>
-  )
-}
+const Clients = () => (
+<section className={`${styles.flexCenter} my-4`}>
+  <div  className={`${styles.flexCenter} flex-wrap w-full`}>
+    {clients.map((client)=>(
+      <div key={client.id}>
+        <img src={client.logo} alt="client" className='w-[50%] h-[50%] flex justify-between flex-col items-center' />
+      </div>
+    ))
+
+    }
+  </div>
+
+</section>
+)
 
 export default Clients
