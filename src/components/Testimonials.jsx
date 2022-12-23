7707dc3f907109a6861e885cdb6f101782ed5624
Berrichi Samir <49416974+Sberrich@users.noreply.github.com>
2022-12-23 17:42:08 +0100

    testimonails finished

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { feedback } from "../constants/constants";
+import styles, { layout } from "../style";
+import FeedbackCard from "./FeedbackCrad";
 
-const Testimonials = () => {
-  return (
-    <div>Testimonials</div>
-  )
-}
+const Testimonials = () => (
+  <section
+    id="clients"
+    className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}
+  >
+    {/* Todo */}
+    <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient"/>
+    <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+      <h1 className={styles.heading2}>
+        What people are <br className="sm:block hidden" />
+        saying about us
+      </h1>
+      <div className="w-full md:mt-0 mt-6">
+        <p className={`${styles.paragraph} text-left max-w-[450px]`}>
+          Everuthing you need to accepts cards payments Everuthing you need to
+          accepts cards payments
+        </p>
+      </div>
+    </div>
+    <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
+      {
+        feedback.map((cards)=>(
+          <FeedbackCard key={cards.id} {...cards}/>
+        ))
+      }
 
-export default Testimonials
+    </div>
+  </section>
+);
+
+export default Testimonials;
