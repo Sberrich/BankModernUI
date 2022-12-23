ce1f8b5dc2b2d5de280361ffc38736fb17a7c4f2
Berrichi Samir <49416974+Sberrich@users.noreply.github.com>
2022-12-23 17:39:52 +0100

    Button finished

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-const Button = () => {
+
+const Button = ({styles}) => {
   return (
-    <div>Button</div>
+    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium rounded-[10px] text-[18px] text-primary outline-none ${styles}`}>Get Started</button>
   )
 }
 
